4c3ea3aaf89d3ce1c9ab1eaccf12d1e3e02e2290
Dan Watson <dcwatson@gmail.com>
2020-07-01 22:03:12 -0400

    Documentation, PZipKey trait, one-shot API

---
+++ README.md
@@ -0,0 +1,6 @@
+# pz
+
+[![PZip](https://img.shields.io/crates/v/pzip.svg)](https://crates.io/crates/pzip)
+[![Docs](https://docs.rs/pzip/badge.svg)](https://docs.rs/pzip)
+
+Implementation of the [PZip format](https://github.com/imsweb/pzip) in Rust.
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate clap;
 
 use clap::{App, Arg};
-use pzip::PZip;
-use std::fs::{File, remove_file};
+use pzip::{Algorithm, Compression, PZip, Password};
+use std::fs::{remove_file, File};
 
 fn main() {
     let matches = App::new("pz")
@@ -20,9 +20,7 @@ fn main() {
                 .takes_value(true)
                 .help("Password for decryption"),
         )
-        .arg(
-            Arg::with_name("keep").short("k").help("Keep input files")
-        )
+        .arg(Arg::with_name("keep").short("k").help("Keep input files"))
         .arg(Arg::with_name("FILE").multiple(true))
         .get_matches();
     let keep = matches.occurrences_of("keep") > 0;
@@ -34,12 +32,21 @@ fn main() {
         if f.ends_with(".pz") {
             let outname = f.replace(".pz", "");
             let mut outfile = File::create(outname).expect("could not create output file");
-            PZip::decrypt(&mut infile, &mut outfile, password.as_bytes()).expect("error decrypting");
+            PZip::decrypt_to(&mut infile, &mut outfile, password.as_bytes())
+                .expect("error decrypting");
         } else {
             let mut outname = String::from(f);
             outname.push_str(".pz");
             let mut outfile = File::create(outname).expect("could not create output file");
-            PZip::encrypt(&mut infile, &mut outfile, password.as_bytes()).expect("error encrypting");
+            let key = Password(password);
+            PZip::encrypt_to(
+                &mut infile,
+                &mut outfile,
+                Algorithm::AesGcm256,
+                &key,
+                Compression::None,
+            )
+            .expect("error encrypting");
         }
         if !keep {
             remove_file(f).ok();
